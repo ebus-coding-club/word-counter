07a61755a12d94485b0d3c553936dbc3a63f5c68
LemonPi314 <49930425+LemonPi314@users.noreply.github.com>
2021-10-26 14:35:59 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+
+
+from collections import Counter
+import matplotlib.pyplot as plt
+
+
+# Parse text into words and remove whitespace and punctuation, except apostrophes
+def parse_words(text: str) -> list[str]:
+    punctuation = r"""!"#$%&()*+,-./:;<=>?@[\]^_`{|}~"""
+    text = text.casefold().replace('\n', ' ')
+    translator = str.maketrans(punctuation, ' ' * len(punctuation))
+    text = text.translate(translator)
+    return text.split()
+
+
+# Parse text into letters and remove whitespace and punctuation
+def parse_letters(text: str) -> list[str]:
+    punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+    text = text.casefold().replace('\n', '')
+    translator = str.maketrans(punctuation, ' ' * len(punctuation))
+    text = text.translate(translator)
+    text = text.replace(' ', '')
+    return list(text)
+
+
+def main():
+    # Read the contents of the input file into a variable
+    with open('input.txt', 'r') as file:
+        text = file.read()
+    # Parse the text
+    parsed = parse_letters(text)
+    # Count the frequency of unique items in the text
+    count = Counter(parsed)
+    # Sort the dictionary from greatest to least
+    count = dict(sorted(count.items(), key=lambda item: item[1], reverse=True))
+    # Create x and y axes for a graph
+    x = [i for i in count.keys()]
+    y = [i for i in count.values()]
+    # Create a graph with matplotlib
+    plt.bar(x, y)
+    # Show the graph
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Word Counter
+By [EBUS Coding Club](https://github.com/ebus-coding-club)
+
+Count the frequency of letters or words in a text file and show a graph.
+## Requirements
+- [Python 3.9](https://www.python.org/downloads/) or higher
+- [`matplotlib`](https://pypi.org/project/matplotlib)
+## Usage
+Download the source code and unzip the downloaded file. Run `pip install -r requirements.txt` in the source code directory to install the required packages. Create a text file in the same directory as `main.py` named `input.txt` and fill it with text you want to analyze. Run the script in an IDE of your choice or with `python main.py`.
+## Objective
+Given a text file, count the frequency (number of occurrences) of either letters or words, and show a bar graph to visualize the results. Do not include whitespace or punctuation in the results, with the exception of apostrophes that are inside words.
+## Next Steps
+- Add command line arguments for input file path and other options 
+- Add timers for significant steps to diagnose performance
+- Optimize speed and memory usage
+- Anything else you can think of to improve the script
+## License
+[MIT License](https://choosealicense.com/licenses/mit/)
